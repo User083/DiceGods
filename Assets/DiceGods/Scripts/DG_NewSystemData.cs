8bd977e84b09c063daede62b5eb4c8c18c29b99e
TeliStaffs <97461050+User083@users.noreply.github.com>
2023-03-09 14:54:30 +0000

    Attribute Tests

--- Assets/DiceGods/Scripts/DG_NewSystemData.cs
+++ Assets/DiceGods/Scripts/DG_NewSystemData.cs
@@ -1,47 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
-//[System.Serializable]
+[System.Serializable]
 
 
 public class DG_NewSystemData : ScriptableObject
 {
-        
-    [Header("Basic")]
+
     public string SystemName;
+    public List<Attribute> attributes = new List<Attribute>();
+    private List<Race> races;
+    private List<Class> classes;
+    private List<CoreStat> coreStats;
 
-    [Header("System Settings")]
-    [SerializeField] public bool usesAttributes;
-    [SerializeField] public bool usesClasses;
-    [SerializeField] public bool usesLevels;
-    [SerializeField] public bool usesRaces;
-    [SerializeField] public bool usesDamageTypes;
-
-    [Header("Core Character Stats")]
-    [SerializeField] public bool health;
-    [SerializeField] public bool mana;
-    [SerializeField] public bool stamina;
-    [SerializeField] public bool armour;
-    [SerializeField] public bool defence;
-
-
-    //[Header("Attributes")]
-    public List<Attribute> Attributes;
-
-    //[Header("Damage Types")]
-    [SerializeField] private List<DamageType> DamageTypes;
-
-  public DG_NewSystemData() 
+    
+    public DG_NewSystemData()
     {
-        //SystemName = "New System";
-        //Attributes = new List<Attribute>();
-        //DamageTypes= new List<DamageType>();
+        
     }
 
-   private void enableSection(bool option)
-    {
-
-    }
 }
